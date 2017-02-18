b606e56b20b28c888dca67f0c533ece06c7a50ed
Hohenheim <zxnzysj@163.com>
2017-02-18 20:56:41 +0800

    Update TailInsert.py

--- DataStructures/LinkedList/TailInsert.py
+++ DataStructures/LinkedList/TailInsert.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 
+#stupid
 def Insert(head, data):
     if head:
         node = head
@@ -9,3 +10,12 @@ def Insert(head, data):
     else:
         head = Node(data)
     return head
+
+#smart
+def Insert(head, data):
+    node = head
+    while node.next:
+        node = node.next
+    node.next = Node(data)
+    return head
+    
